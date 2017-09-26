25e5dfb88962ff66cdb322b2affe1c9276f96a0f
YingzhouLi <yingzhouli0417@gmail.com>
2017-09-26 11:50:28 -0400

    Updated mendeleydesktop formula to support both arch 32 and 64 bit

--- Formula/mendeleydesktop.rb
+++ Formula/mendeleydesktop.rb
@@ -5,17 +5,21 @@
 class Mendeleydesktop < Formula
   desc "Mendeley Desktop"
   homepage "https://www.mendeley.com"
-  url "https://www.mendeley.com/client/get/100-2/"
   version "1.17.11"
-  sha256 "dc9fce0da7f49fb383f1ba8646ebd4cb43fb4f133b32e2c25803a3359d8da68f"
 
-  depends_on :arch => :x86_64
+  if MacOS.prefer_64_bit?
+    url "https://www.mendeley.com/client/get/100-2/"
+    sha256 "dc9fce0da7f49fb383f1ba8646ebd4cb43fb4f133b32e2c25803a3359d8da68f"
+  else
+    url "https://www.mendeley.com/client/get/100-1/"
+    sha256 "0274aa2e7f915f62c607486591560b9a2aed3f4cf56ad226386a411f10369f1d"
+  end
 
   bottle :unneeded
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink("#{libexec}/mendeleydesktop" => "mendeleydesktop")
+    bin.install_symlink("#{libexec}/bin/mendeleydesktop" => "mendeleydesktop")
   end
 
 end
